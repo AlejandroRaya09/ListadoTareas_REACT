808b7094cacf675f925d00b602cce469fcfaaece
AlejandroRaya09 <alejandroraya09@hotmail.com>
2023-04-20 00:49:38 -0600

    Obtener y guardar en localstorage los valores

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-
-import 'semantic-ui-css/semantic.min.css'
-import Container from './components/Container';
-import Header from './components/Header';
-import InputTask from './components/InputTask';
-
+import { useEffect, useState } from "react";
+import "semantic-ui-css/semantic.min.css";
+import Container from "./components/Container";
+import Header from "./components/Header";
+import InputTask from "./components/InputTask";
 
 function App() {
+  //PASAR LAS TAREAS A LOCALSTORGE
+  let initialTasks = JSON.parse(localStorage.getItem("tasks"));
+
+  if (!initialTasks) {
+    initialTasks = [];
+  }
+
+  const [tasks, setTasks] = useState(initialTasks);
+
+  useEffect(()=>{
+    if(initialTasks){
+      localStorage.setItem("tasks", JSON.stringify(tasks))
+    }else{
+      localStorage.setItem("tasks", JSON.stringify([]))
+    }
+
+  }, [initialTasks,tasks])
+
+  const crearTask = (task) => {
+    setTasks([...tasks, task]);
+  };
+
+
   return (
-    <Container> 
-    <Header/>
-    <InputTask/>
-  </Container>
-  )
+    <Container>
+      <Header />
+      <InputTask createTask={crearTask} />
+    </Container>
+  );
 }
 export default App;
-  
--- src/components/InputTask.js
+++ src/components/InputTask.js
@@ -1,59 +1,107 @@
-import { Select, Input, Button, Grid, Header, Icon } from "semantic-ui-react"
-import { useState } from "react"
-
+import { Select, Input, Button, Grid, Header, Icon } from "semantic-ui-react";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const options = [
-    {key:"deporte", text:"Deporte", value:"deporte"},
-    {key:"casa", text:"Casa", value:"casa"},
-    {key:"oficina", text:"Oficina", value:"oficina"},
-    {key:"otra", text:"Otra", value:"otra"}
-]
+  { key: "deporte", text: "Deporte", value: "deporte" },
+  { key: "casa", text: "Casa", value: "casa" },
+  { key: "oficina", text: "Oficina", value: "oficina" },
+  { key: "otra", text: "Otra", value: "otra" },
+];
 
-export default function InputTask() {
-
-const [task, useTask] = useState({
+export default function InputTask(props) {
+  const [task, setTask] = useState({
     idTask: "",
     taskName: "",
-    categoryName: ""
-});
-const [error, setError] = useState(false)
+    categoryTask: "",
+  });
 
-const onChangeTask = (e) =>{
+  const [error, setError] = useState(false);
 
+  const { createTask } = props;
+
+  //ASIGNAR VALORES AL TASK
+  const onChangeTask = (e) => {
+    setTask({
+      ...task,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onChangeCategoryTask = (e, data) => {
+    setTask({
+      ...task,
+      [data.name]: data.value,
+    });
+  };
+
+  const onSubmitTask = (e) => {
+    //TODO *QUE NO RECARGUE LA PAGINA*
+    e.preventDefault(); //NO RECARGAR PAGINA
+
+    //VALIDACION
+    if (task.taskName.trim() === "" || task.categoryTask.trim() === "") {
+      setError(true);
+      return;
+    }
+
+    //ELIMINAR MENSAJE PREVIO SI EXISTE
+    setError(false);
+
+    //ASIGNAR ID
+    task.idTask = uuidv4();
+
+    //CREAR TAREA
+    createTask(task);
+
+    //LIMPIAR INPUTS
+    setTask({
+      idTask: "",
+      taskName: "",
+      categoryTask: "",
+    });
+  };
+
+  return (
+    <>
+      <Grid centered columns={2}>
+        <Input type="text" action>
+          <Input
+            size="small"
+            icon="add"
+            placeholder="Escribe tu tarea..."
+            iconPosition="left"
+            name="taskName"
+            value={task.taskName}
+            onChange={onChangeTask}
+            //! onChange={()=> setError(true)} VER MENSAJE DE ERROR
+          />
+
+          <Select
+            compact
+            options={options}
+            className="select-from-task"
+            placeholder="Categoria"
+            name="categoryTask"
+            value={task.categoryTask}
+            onChange={onChangeCategoryTask}
+          />
+
+          <Button type="submit" color="violet" onClick={onSubmitTask}>
+            Añadir Tarea
+          </Button>
+        </Input>
+      </Grid>
+
+      {error && (
+        <Grid centered>
+          <Header as="h4" color="red" className="alert-error-form">
+            <Icon name="close" />
+            <Header.Content>La tarea es obligartoria</Header.Content>
+            <Icon name="close" />
+          </Header>
+        </Grid>
+      )}
+    </>
+  );
 }
-    return (
-        <>
-            <Grid centered columns={2}>
-                <Input type="text" action>
-                    <Input size="small"
-                     icon="add" 
-                     placeholder="Escribe tu tarea..." 
-                     iconPosition="left" 
-                     name="taskName" 
-                     value={task.taskName}
-                     onChange={onChangeTask}/>
-
-
-                    <Select compact
-                     options={options} 
-                     className="select-from-task" 
-                     name="categoryTask" 
-                     placeholder="Categoria"
-                     value={task.categoryName}/>
-
-                    <Button type="submit" 
-                    color="violet">Añadir Tarea</Button>
-                </Input>
-            </Grid>
-            {error && (
-                <Grid centered>
-                    <Header as ="h4" color="red" className="alert-error-form">
-                    <Icon name="close" />
-                    <Header.Content>La tearea es obligartoria</Header.Content>
-                    <Icon name="closed" />
-                    </Header>
-                </Grid>
-            )}
-        </>
-    )
-};
